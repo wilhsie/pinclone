8ddc035da2cc1b222f5fdd6ac8d5c51fdf4d9047
wilhsie <wjhsie@gmail.com>
2016-03-02 00:23:13 -0500

    changes to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # Changed to true to load compile background image asset
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -79,4 +80,5 @@ Rails.application.configure do
 
   # Required for Heroku
   config.action_mailer.default_url_options = { host: 'lifeinslic.es' }
+
 end
