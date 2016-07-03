ae9e7e49ad6caaa37f63688b1b6d27d5a3859063
wilhsie <wjhsie@gmail.com>
2016-07-03 14:42:49 -0400

    added geolocation error in pages.js

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,8 +1,12 @@
 function getGeoLocation() {
-  navigator.geolocation.getCurrentPosition(setGeoCookie);
+  navigator.geolocation.getCurrentPosition(setGeoCookie, errorGeoCookie);
 }
 
 function setGeoCookie(position) {
   var cookie_val = position.coords.latitude + "|" + position.coords.longitude;
   document.cookie = "lat_lng=" + escape(cookie_val);
+}
+
+function errorGeoCookie(){
+  alert("Please allow location services to view this site's content");
 }
