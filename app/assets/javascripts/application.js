5f885399f692840e615bb4345beb19cd1723d7dc
wilhsie <wjhsie@gmail.com>
2016-07-03 14:16:47 -0400

    break asset pipeline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,11 +15,3 @@
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
-function getGeoLocation() {
-  navigator.geolocation.getCurrentPosition(setGeoCookie);
-}
-
-function setGeoCookie(position) {
-  var cookie_val = position.coords.latitude + "|" + position.coords.longitude;
-  document.cookie = "lat_lng=" + escape(cookie_val);
-}
