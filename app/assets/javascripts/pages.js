91982e71e1bb3ad5bccd41e9cbbdf2ba4e563f7b
wilhsie <wjhsie@gmail.com>
2016-07-26 02:19:21 -0400

    removed geoError in pages.js

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,15 +1,8 @@
 function getGeoLocation() {
-  navigator.geolocation.getCurrentPosition(setGeoCookie, errorGeoCookie, {maximumAge: 60000});
+  navigator.geolocation.getCurrentPosition(setGeoCookie, {maximumAge: 60000});
 }
 
 function setGeoCookie(position) {
   var cookie_val = position.coords.latitude + "|" + position.coords.longitude;
   document.cookie = "lat_lng=" + escape(cookie_val);
 }
-
-function errorGeoCookie(err){
-  if(err.code == 1){
-    // user said no
-    console.log("We can't show you cool stuff if location services is off :[");
-  }
-} 
